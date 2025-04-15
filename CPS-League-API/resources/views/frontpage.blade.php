1ee159b216e012818ff61f110dcebf023adbe13b
Nikolai Bruun <nibru23@student.sdu.dk>
2025-04-15 12:04:42 +0200

    Added controllers, css files, views and routes

---
+++ CPS-League-API/resources/views/frontpage.blade.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> SuperMegaOvernice-Softtek-frontpage </title>
+    <link rel="stylesheet" href="{{asset('/css/frontpage.css')}}">
+</head>
+
+<body>
+<header class="header">
+    <div class="site-name">League of Dudes</div>
+    <div class="search-bar">
+        <input type="text" placeholder="Search summoner">
+        <button><span>&#128269;</span></button>
+    </div>
+</header>
+<span>HI THERE!</span>
+
+    <h1> FUCK THEM BIG BOOTY BROS </h1>
+
+
+
+
+
+</body>
+
+
+
+
+
+</html>
+
+
+
+
+
