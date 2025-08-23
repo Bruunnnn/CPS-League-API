33667a23f3ae2521a58c33a09286f7c7806fa253
Jakob Strate <jastr23@student.sdu.com>
2025-08-23 21:12:07 +0200

    added ranked png's to the ranked.blade so it shows when player is searched on

--- CPS-League-API/resources/views/partials/ranked.blade.php
+++ CPS-League-API/resources/views/partials/ranked.blade.php
@@ -10,18 +10,56 @@
             <span class="ranked-label">Ranked Solo:</span>
         </div>
         <div class="ranked-col value">
-            <span class="ranked-value">{{ $rankedMap['solo'] ?? 'Unranked' }}</span>
+            @php
+            $soloRankName = $rankedMap['solo'] ?? null;
+            // Extract only the first word (the tier)
+            $soloTier = $soloRankName ? explode(' ', $soloRankName)[0] : null;
+            $soloImage = $soloTier ? $soloTier . '.png' : 'Unranked.png';
+            @endphp
+
+            @if($soloRankName)
+            <img src="{{ asset('img/' . $soloImage) }}"
+                 alt="{{ $soloTier }}"
+                 class="ranked-icon"
+                 style="width: 125px; height: 125px; vertical-align: middle; margin-right: 6px;">
+            <span class="ranked-value">{{ $soloRankName }}</span>
+            @else
+            <img src="{{ asset('img/Unranked.png') }}"
+                 alt="Unranked"
+                 class="ranked-icon"
+                 style="width: 125px; height: 125px; vertical-align: middle; margin-right: 6px;">
+            <span class="ranked-value">Unranked</span>
+            @endif
         </div>
         <div class="ranked-col points">
             <span class="ranked-points">{{ $soloRank ? 'LP: ' . $soloRank->leaguePoints : 'LP: ??' }}</span>
         </div>
     </div>
+
     <div class="ranked-mode-block">
         <div class="ranked-col label">
             <span class="ranked-label">Ranked Flex:</span>
         </div>
         <div class="ranked-col value">
-            <span class="ranked-value">{{ $rankedMap['flex'] ?? 'Unranked' }}</span>
+            @php
+            $flexRankName = $rankedMap['flex'] ?? null;
+            $flexTier = $flexRankName ? explode(' ', $flexRankName)[0] : null;
+            $flexImage = $flexTier ? $flexTier . '.png' : 'Unranked.png';
+            @endphp
+
+            @if($flexRankName)
+            <img src="{{ asset('img/' . $flexImage) }}"
+                 alt="{{ $flexTier }}"
+                 class="ranked-icon"
+                 style="width: 125px; height: 125px; vertical-align: middle; margin-right: 6px;">
+            <span class="ranked-value">{{ $flexRankName }}</span>
+            @else
+            <img src="{{ asset('img/Unranked.png') }}"
+                 alt="Unranked"
+                 class="ranked-icon"
+                 style="width: 125px; height: 125px; vertical-align: middle; margin-right: 6px;">
+            <span class="ranked-value">Unranked</span>
+            @endif
         </div>
         <div class="ranked-col points">
             <span class="ranked-points">{{ $flexRank ? 'LP: ' . $flexRank->leaguePoints : 'LP: ??' }}</span>
