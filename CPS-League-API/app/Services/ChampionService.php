170b677d54e0f016f10d416cfb0f699d9ecf615f
Jakob Strate <jastr23@student.sdu.com>
2025-09-06 13:18:17 +0200

    smaller changes to championservice

--- CPS-League-API/app/Services/ChampionService.php
+++ CPS-League-API/app/Services/ChampionService.php
@@ -1,24 +1,39 @@
 <?php
 
 namespace App\Services;
+
 use Illuminate\Support\Facades\Http;
 use App\Models\Champion;
 
 class ChampionService
 {
+    protected GeneralService $generalService;
+
+    public function __construct(GeneralService $generalService)
+    {
+        $this->generalService = $generalService;
+    }
+
     public function storeAllChampions()
     {
-        $response = Http::withoutVerifying()->get("https://ddragon.leagueoflegends.com/cdn/15.16.1/data/en_US/champion.json");
+        // Fetch the latest patch dynamically
+        $latestPatch = $this->generalService->getLatestPatch();
+
+        // Get champion data from Data Dragon
+        $response = Http::withoutVerifying()
+            ->get("https://ddragon.leagueoflegends.com/cdn/{$latestPatch}/data/en_US/champion.json");
+
         $championData = $response->json()['data'] ?? [];
 
-        foreach ($championData as $champion){
+        // Store or update champion info
+        foreach ($championData as $champion) {
             Champion::updateOrCreate(
                 ['key' => $champion['key']],
                 [
                     'name' => $champion['name'],
                     'title' => $champion['title'],
                     'blurb' => $champion['blurb'],
-                    'image_url' => "https://ddragon.leagueoflegends.com/cdn/15.16.1/img/champion/{$champion['id']}.png",
+                    'image_url' => "https://ddragon.leagueoflegends.com/cdn/{$latestPatch}/img/champion/{$champion['id']}.png",
                 ]
             );
         }
